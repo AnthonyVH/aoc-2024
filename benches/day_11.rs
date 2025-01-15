f99cfb923ddcc4c359bbbd0583e8a22bb8902fe8
Anthony Van Herrewege <anthony@anthonyvh.com>
2025-01-15 19:20:00 +0100

    Add iterative solution to day 11

--- benches/day_11.rs
+++ benches/day_11.rs
@@ -1,3 +1,4 @@
+use aoc_2024::day_11::{Execution, Looping, NUM_BLINKS_B};
 use criterion;
 
 fn bench_part_a(bench: &mut criterion::Criterion) {
@@ -7,24 +8,76 @@ fn bench_part_a(bench: &mut criterion::Criterion) {
     });
 }
 
-fn bench_part_b_sequential(bench: &mut criterion::Criterion) {
+fn bench_part_b(bench: &mut criterion::Criterion) {
     let input: String = util::read_resource("day_11.txt").unwrap();
-    bench.bench_function("Day 11 - Part B - Sequential", |b| {
-        b.iter(|| aoc_2024::day_11::part_b(&input, false))
+    bench.bench_function("Day 11 - Part B", |b| {
+        b.iter(|| aoc_2024::day_11::part_b(&input))
     });
 }
 
-fn bench_part_b_parallel(bench: &mut criterion::Criterion) {
+fn bench_part_b_sequential_iterative(bench: &mut criterion::Criterion) {
     let input: String = util::read_resource("day_11.txt").unwrap();
-    bench.bench_function("Day 11 - Part B - Parallel", |b| {
-        b.iter(|| aoc_2024::day_11::part_b(&input, true))
+    bench.bench_function("Day 11 - Part B - Sequential & Iterative", |b| {
+        b.iter(|| {
+            aoc_2024::day_11::parse_and_solve(
+                &input,
+                NUM_BLINKS_B,
+                Execution::Sequential,
+                Looping::Iterative,
+            )
+        })
+    });
+}
+
+fn bench_part_b_sequential_recursive(bench: &mut criterion::Criterion) {
+    let input: String = util::read_resource("day_11.txt").unwrap();
+    bench.bench_function("Day 11 - Part B - Sequential & Recursive", |b| {
+        b.iter(|| {
+            aoc_2024::day_11::parse_and_solve(
+                &input,
+                NUM_BLINKS_B,
+                Execution::Sequential,
+                Looping::Recursive,
+            )
+        })
+    });
+}
+
+fn bench_part_b_parallel_iterative(bench: &mut criterion::Criterion) {
+    let input: String = util::read_resource("day_11.txt").unwrap();
+    bench.bench_function("Day 11 - Part B - Parallel & Iterative", |b| {
+        b.iter(|| {
+            aoc_2024::day_11::parse_and_solve(
+                &input,
+                NUM_BLINKS_B,
+                Execution::Parallel,
+                Looping::Iterative,
+            )
+        })
+    });
+}
+
+fn bench_part_b_parallel_recursive(bench: &mut criterion::Criterion) {
+    let input: String = util::read_resource("day_11.txt").unwrap();
+    bench.bench_function("Day 11 - Part B - Parallel & Recursive", |b| {
+        b.iter(|| {
+            aoc_2024::day_11::parse_and_solve(
+                &input,
+                NUM_BLINKS_B,
+                Execution::Parallel,
+                Looping::Recursive,
+            )
+        })
     });
 }
 
 criterion::criterion_group!(
     benches,
     bench_part_a,
-    bench_part_b_sequential,
-    bench_part_b_parallel
+    bench_part_b,
+    bench_part_b_sequential_iterative,
+    bench_part_b_sequential_recursive,
+    bench_part_b_parallel_iterative,
+    bench_part_b_parallel_recursive
 );
 criterion::criterion_main!(benches);
--- src/bin/day_11.rs
+++ src/bin/day_11.rs
@@ -8,9 +8,6 @@ fn main() {
 
     {
         let input: String = util::read_resource("day_11.txt").unwrap();
-        println!(
-            "[Day 11 - Part B] {}",
-            aoc_2024::day_11::part_b(&input, false)
-        )
+        println!("[Day 11 - Part B] {}", aoc_2024::day_11::part_b(&input))
     }
 }
