1b2af1e79ea1d082bc0135b570adfe12cb94b6ca
Anthony Van Herrewege <anthony.vanherrewege@zhinst.com>
2025-01-16 22:09:24 +0100

    Massively improved runtime for day 7

--- benches/day_07.rs
+++ benches/day_07.rs
@@ -1,39 +1,18 @@
-use aoc_2024::day_07::Looping;
 use criterion;
 
-fn bench_part_a_iterative(bench: &mut criterion::Criterion) {
+fn bench_part_a(bench: &mut criterion::Criterion) {
     let input: String = util::read_resource("day_07.txt").unwrap();
-    bench.bench_function("Day 07 - Part A - Iterative", |b| {
-        b.iter(|| aoc_2024::day_07::part_a_configurable(&input, Looping::Iterative))
+    bench.bench_function("Day 07 - Part A", |b| {
+        b.iter(|| aoc_2024::day_07::part_a(&input))
     });
 }
 
-fn bench_part_a_recursive(bench: &mut criterion::Criterion) {
+fn bench_part_b(bench: &mut criterion::Criterion) {
     let input: String = util::read_resource("day_07.txt").unwrap();
-    bench.bench_function("Day 07 - Part A - Recursive", |b| {
-        b.iter(|| aoc_2024::day_07::part_a_configurable(&input, Looping::Recursive))
+    bench.bench_function("Day 07 - Part B", |b| {
+        b.iter(|| aoc_2024::day_07::part_b(&input))
     });
 }
 
-fn bench_part_b_iterative(bench: &mut criterion::Criterion) {
-    let input: String = util::read_resource("day_07.txt").unwrap();
-    bench.bench_function("Day 07 - Part B - Iterative", |b| {
-        b.iter(|| aoc_2024::day_07::part_b_configurable(&input, Looping::Iterative))
-    });
-}
-
-fn bench_part_b_recursive(bench: &mut criterion::Criterion) {
-    let input: String = util::read_resource("day_07.txt").unwrap();
-    bench.bench_function("Day 07 - Part B - Recursive", |b| {
-        b.iter(|| aoc_2024::day_07::part_b_configurable(&input, Looping::Recursive))
-    });
-}
-
-criterion::criterion_group!(
-    benches,
-    bench_part_a_iterative,
-    bench_part_a_recursive,
-    bench_part_b_iterative,
-    bench_part_b_recursive
-);
+criterion::criterion_group!(benches, bench_part_a, bench_part_b,);
 criterion::criterion_main!(benches);
--- src/day_07.rs
+++ src/day_07.rs
@@ -1,9 +1,12 @@
 use rayon::prelude::*;
+use smallvec::SmallVec;
+
+const NUM_ELEMENTS: usize = 12;
 
 #[derive(Debug)]
 struct Equation {
     target: u64,
-    values: Vec<u64>,
+    values: SmallVec<[u16; NUM_ELEMENTS]>,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -14,19 +17,29 @@ enum Operator {
 }
 
 impl Operator {
-    fn eval(self, lhs: u64, rhs: u64) -> u64 {
+    fn reverse_eval(self, lhs: u64, rhs: u64) -> Option<u64> {
         match self {
-            Operator::Add => lhs + rhs,
-            Operator::Mult => lhs * rhs,
+            Operator::Add => {
+                assert!(lhs >= rhs); // Guaranteed by _solve_reverse() function.
+                Some(lhs - rhs)
+            }
+            Operator::Mult => match lhs % rhs {
+                0 => Some(lhs / rhs),
+                _ => None,
+            },
             Operator::Concat => {
-                let lhs_shifted = lhs * 10_u64.pow(Operator::_digit_width_base10(rhs));
-                lhs_shifted + rhs
+                const DIVISORS: [u64; 3] = [10, 100, 1000];
+
+                let divisor_idx = util::digit_width_base10(rhs) - 1;
+                assert!(divisor_idx <= 2);
+                let divisor = DIVISORS[divisor_idx as usize];
+
+                match lhs % divisor == rhs {
+                    false => None,
+                    true => Some(lhs / divisor),
+                }
             }
         }
-    }
-
-    fn _digit_width_base10(x: u64) -> u32 {
-        x.ilog10() + 1
     }
 }
 
@@ -34,212 +47,94 @@ impl std::str::FromStr for Equation {
     type Err = std::string::ParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut splits = s.split(": ");
+        let mut ascii = s.as_bytes();
+        let (target, pos) = atoi_simd::parse_any_pos(&ascii).unwrap();
+        ascii = &ascii[pos + 1..]; // Skip colon (not space!).
 
-        Ok(Equation {
-            target: splits.next().unwrap().parse().unwrap(),
-            values: splits
-                .next()
-                .unwrap()
-                .split_whitespace()
-                .map(|e| e.parse().unwrap())
-                .collect(),
-        })
+        let mut values = SmallVec::new();
+        while ascii.len() > 0 {
+            let (value, pos) = atoi_simd::parse_any_pos(&ascii[1..]).unwrap();
+            ascii = &ascii[pos + 1..]; // Also skip over initial space.
+            values.push(value)
+        }
+
+        Ok(Equation { target, values })
     }
 }
 
 impl Equation {
-    const MAX_STACK_STACK_SIZE: usize = 20;
-
-    fn _recurse_solution(&self, result: u64, values: &[u64], operators: &[Operator]) -> bool {
-        if values.is_empty() {
-            return result == self.target;
-        } else if result > self.target {
-            return false; // Result too large to ever become a solution.
+    fn _solve_reversed(&self, target: u64, values: &[u16], operators: &[Operator]) -> bool {
+        if values.len() == 1 {
+            return values[0] as u64 == target;
+        } else if target < values[values.len() - 1] as u64 {
+            // If target is smaller than a value, then this can never solve.
+            return false;
         }
 
         for op in operators.iter() {
-            let next_result = op.eval(result, values[0]);
-            if self._recurse_solution(next_result, &values[1..], operators) {
-                return true; // Found a solution.
+            match op.reverse_eval(target, values[values.len() - 1] as u64) {
+                None => continue,
+                Some(next_target) => {
+                    if self._solve_reversed(next_target, &values[..values.len() - 1], operators) {
+                        return true;
+                    }
+                }
             }
         }
 
         false
     }
 
-    /// Stack-based implementation of the recursive implementation. It's faster.
-    fn _iterate_solution(&self, operators: &[Operator]) -> bool {
-        struct State {
-            result: u64,
-            op_idx: u8,
-            value_idx: u8,
-        }
-
-        let mut stack: Vec<State> = Vec::with_capacity(Self::MAX_STACK_STACK_SIZE);
-        let mut found_solution = false;
-
-        // Prime stack with first value.
-        stack.push(State {
-            result: self.values[0],
-            op_idx: 0,
-            value_idx: 1,
-        });
-
-        while !stack.is_empty() {
-            let state = stack.last_mut().unwrap();
-
-            if state.op_idx as usize == operators.len() {
-                // Already evaluated all operators for this value.
-                stack.pop();
-            } else if state.value_idx as usize == self.values.len() {
-                // Remember state.
-                found_solution = state.result == self.target;
-
-                // No more values to process.
-                stack.pop();
-
-                if found_solution {
-                    break; // Solution was found, stop searching.
-                }
-            } else {
-                // Try the next operator.
-                let op = operators[state.op_idx as usize];
-                let value = self.values[state.value_idx as usize];
-                let next_result = op.eval(state.result, value);
-                let next_value_idx = state.value_idx + 1;
-
-                // Prepare next iteration of this "stack frame".
-                state.op_idx += 1;
-
-                if next_result <= self.target {
-                    // Potential solution, push state to evaluate next value.
-                    stack.push(State {
-                        result: next_result,
-                        op_idx: 0,
-                        value_idx: next_value_idx as u8,
-                    });
-                }
-            }
-        }
-
-        found_solution
-    }
-
-    fn solvable(&self, operators: &[Operator], looping: Looping) -> bool {
-        match looping {
-            Looping::Recursive => self._solvable_iterative(operators),
-            Looping::Iterative => self._solvable_recursive(operators),
-        }
-    }
-
-    fn _solvable_recursive(&self, operators: &[Operator]) -> bool {
-        // More compiler hints == faster.
-        assert!(self.values.len() >= 2);
-
-        let possible = self._recurse_solution(self.values[0], &self.values[1..], operators);
-        log::debug!("{:?} => {}", self, possible);
-        possible
-    }
-
-    fn _solvable_iterative(&self, operators: &[Operator]) -> bool {
-        let possible = self._iterate_solution(operators);
-        log::debug!("{:?} => {}", self, possible);
-        possible
+    fn solvable(&self, operators: &[Operator]) -> bool {
+        // Solve from "back to front". I.e. start with the expected value and
+        // work back through the list of values until the first one is reached
+        // and matches the remaining expected value.
+        self._solve_reversed(self.target, &self.values, operators)
     }
 }
 
-#[derive(Copy, Clone)]
-pub enum Looping {
-    Iterative,
-    Recursive,
-}
+fn solve(input: &str, operators: &[Operator]) -> u64 {
+    // Collect into vector to allow rayon to efficiently split objects across
+    // its workers.
+    let equations: Vec<_> = input.lines().map(|e| e.parse().unwrap()).collect();
 
-pub fn part_a_configurable(input: &str, looping: Looping) -> u64 {
-    let problem: Vec<Equation> = input.lines().map(|e| e.parse().unwrap()).collect();
-    let operators = [Operator::Add, Operator::Mult];
-
-    problem
+    equations
         .par_iter()
-        .filter(|eq| eq.solvable(&operators, looping))
-        .map(|e| e.target)
-        .sum()
-}
-
-pub fn part_b_configurable(input: &str, looping: Looping) -> u64 {
-    let problem: Vec<Equation> = input.lines().map(|e| e.parse().unwrap()).collect();
-    let operators = [Operator::Add, Operator::Mult, Operator::Concat];
-
-    problem
-        .par_iter()
-        .filter(|eq| eq.solvable(&operators, looping))
+        .filter(|eq: &&Equation| eq.solvable(operators))
         .map(|e| e.target)
         .sum()
 }
 
 pub fn part_a(input: &str) -> u64 {
-    part_a_configurable(input, Looping::Iterative)
+    let operators = [Operator::Mult, Operator::Add];
+    solve(input, &operators)
 }
 
 pub fn part_b(input: &str) -> u64 {
-    part_b_configurable(input, Looping::Iterative)
+    let operators = [Operator::Concat, Operator::Mult, Operator::Add];
+    solve(input, &operators)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day_07::Looping;
-
-    const EXPECTED_A: u64 = 3749;
-    const EXPECTED_B: u64 = 11387;
-
     #[test]
-    fn example_a_recursive() {
+    fn example_a() {
         util::run_test(|| {
+            let expected: u64 = 3749;
             assert_eq!(
-                crate::day_07::part_a_configurable(
-                    &util::read_resource("example_07.txt").unwrap(),
-                    Looping::Recursive
-                ),
-                EXPECTED_A
+                crate::day_07::part_a(&util::read_resource("example_07.txt").unwrap()),
+                expected
             );
         });
     }
 
     #[test]
-    fn example_a_iterative() {
+    fn example_b() {
         util::run_test(|| {
+            let expected: u64 = 11387;
             assert_eq!(
-                crate::day_07::part_a_configurable(
-                    &util::read_resource("example_07.txt").unwrap(),
-                    Looping::Iterative
-                ),
-                EXPECTED_A
-            );
-        });
-    }
-
-    #[test]
-    fn example_b_recursive() {
-        util::run_test(|| {
-            assert_eq!(
-                crate::day_07::part_b_configurable(
-                    &util::read_resource("example_07.txt").unwrap(),
-                    Looping::Recursive
-                ),
-                EXPECTED_B
-            );
-        });
-    }
-
-    #[test]
-    fn example_b_iterative() {
-        util::run_test(|| {
-            assert_eq!(
-                crate::day_07::part_b_configurable(
-                    &util::read_resource("example_07.txt").unwrap(),
-                    Looping::Iterative
-                ),
-                EXPECTED_B
+                crate::day_07::part_b(&util::read_resource("example_07.txt").unwrap()),
+                expected
             );
         });
     }
